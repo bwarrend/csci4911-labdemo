5ff99dc9a5e17711e1e456bc20c340e27103c8bc
Brett Warrender <bwarrend@my.hpu.edu>
2020-01-15 17:12:04 -1000

    Added method

--- hello.java
+++ hello.java
@@ -3,7 +3,12 @@ package hello;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        greeting("hello");
+    }
+    
+    
+    public static void greeting(String msg){
+        System.out.println(msg);
     }
     
 }
